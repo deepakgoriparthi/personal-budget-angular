9b2504af14147cbfd7f8efdd8fc38930ab2996e3
deepakgoriparthi <deepakgoriparthi@gmail.com>
2024-09-22 23:06:00 -0400

    Updated Instructions

--- server/server.js
+++ server/server.js
@@ -5,32 +5,11 @@ const cors=require("cors");
 
 const budget = require('./budget-data.json');
 app.use(cors());
-// app.use('/', express.static('public'));
-//const budget= {
-//    myBudget: [
-//    {
-//        title: 'Eat out',
-//        budget: 25
-//    },
-//    {
-//        title: 'Rent',
-//        budget: 375
-//    },
-//    {
-//        title: 'Grocery',
-//        budget: 110
-//    },
-// ]
-//};
-// app.use("/",express.static('public'))
-//app.get('/hello', (req,res) => {
-//    res.send('hello world!');
-//});
 
 app.get('/budget',(req,res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
